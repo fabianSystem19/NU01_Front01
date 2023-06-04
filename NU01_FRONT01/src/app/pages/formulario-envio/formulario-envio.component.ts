189136967e36b1853d4b748f0c9524194dad37fd
Valen <vvisgal2@gmail.com>
2023-06-04 17:58:13 -0500

    Actualización formulario envío

--- NU01_FRONT01/src/app/pages/formulario-envio/formulario-envio.component.ts
+++ NU01_FRONT01/src/app/pages/formulario-envio/formulario-envio.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-envio',
   templateUrl: './formulario-envio.component.html',
   styleUrls: ['./formulario-envio.component.css']
 })
-
-export class FormularioEnvioComponent {
-
+export class FormularioEnvioComponent implements OnInit {
+  
+  formulario: FormGroup = new FormGroup({});
+  
+  ngOnInit() {
+    this.formulario = new FormGroup({
+      correo: new FormControl(''),
+      correos_exclusivos: new FormControl(''),
+      datos: new FormGroup({
+        nombre: new FormControl(''),
+        apellido: new FormControl(''),
+        pais: new FormControl(''),
+        departamento: new FormControl(''),
+        ciudad: new FormControl(''),
+        direccion: new FormControl('')
+      }),
+      facturacion: new FormGroup({
+        cedula: new FormControl(''),
+        telefono: new FormControl('')
+      })
+    });
+  }
+  
+  enviarFormulario(): void {
+    if (this.formulario.valid) {
+      console.log(this.formulario.value);
+    }
+  }
 }
