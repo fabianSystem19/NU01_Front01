d45f9edefa7ad5787477bec1d703feb478ec8aec
Valen <vvisgal2@gmail.com>
2023-06-04 13:24:32 -0500

    Creación carpetas de pages

--- NU01_FRONT01/src/app/app.module.ts
+++ NU01_FRONT01/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioEnvioComponent } from './formulario-envio/formulario-envio.component';
+import { ValidacionCarritoComponent } from './pages/validacion-carrito/validacion-carrito.component';
+import { FormularioPagosComponent } from './pages/formulario-pagos/formulario-pagos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioEnvioComponent,
+    ValidacionCarritoComponent,
+    FormularioPagosComponent
   ],
   imports: [
     BrowserModule,
