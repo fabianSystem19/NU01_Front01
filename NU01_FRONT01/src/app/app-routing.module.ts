2ecb132dd45502851727231cb1603184f5827f77
Valen <vvisgal2@gmail.com>
2023-06-04 15:05:15 -0500

    Rutas de componentes

--- NU01_FRONT01/src/app/app-routing.module.ts
+++ NU01_FRONT01/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioEnvioComponent } from './pages/formulario-envio/formulario-envio.component';
 import { ValidacionCarritoComponent } from './pages/validacion-carrito/validacion-carrito.component';
+import { FormularioPagosComponent } from './pages/formulario-pagos/formulario-pagos.component';
+
 
 const routes: Routes = [
   {path: "formulario-envio", component: FormularioEnvioComponent},
+  {path: "carrito", component: ValidacionCarritoComponent},
+  {path: "pago", component: FormularioPagosComponent}
 
 ];
 
