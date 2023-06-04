6e3e51a33fd5d6de620b53f5153386121caa2f43
Valen <vvisgal2@gmail.com>
2023-06-04 14:15:41 -0500

    Componente Header

--- NU01_FRONT01/src/app/app.module.ts
+++ NU01_FRONT01/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
