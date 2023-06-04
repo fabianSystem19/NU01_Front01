1e7663a15436a941261f9e52219a35d8e44ff6cf
Valen <vvisgal2@gmail.com>
2023-06-04 13:24:57 -0500

    creación carpetas

---
+++ NU01_FRONT01/src/app/pages/formulario-envio/formulario-envio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-envio',
+  templateUrl: './formulario-envio.component.html',
+  styleUrls: ['./formulario-envio.component.scss']
+})
+export class FormularioEnvioComponent {
+
+}
---
+++ NU01_FRONT01/src/app/pages/validacion-carrito/validacion-carrito.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validacion-carrito',
+  templateUrl: './validacion-carrito.component.html',
+  styleUrls: ['./validacion-carrito.component.scss']
+})
+export class ValidacionCarritoComponent {
+
+}
